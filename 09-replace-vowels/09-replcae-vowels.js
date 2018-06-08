ca972a41fa779cb84b1bef2b77c653b92d52b0b7
Ryan Luras <ryanluras@gmail.com>
2018-06-08 11:41:24 -0700

    fixed codewars 09

--- 09-replace-vowels/09-replcae-vowels.js
+++ 09-replace-vowels/09-replcae-vowels.js
@@ -1,14 +1,4 @@
-function isNice(arr){
-  if(arr.length === 0) {
-  return false;
-  }
-  let nice = true;
-  for(i in arr) {
-    if(arr.includes(arr[i]+1) || arr.includes(arr[i]-1)) {
-    } else {
-    nice = false;
-    }
-  }
-  return nice;
-};
-
+function disemvowel(str) {
+  str = str.replace(/[aeiou]/gi, "");
+  return str;
+}
